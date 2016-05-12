bae363ef617730fdb22d9d044482c9ae54670477
Raymond Sapida <ray.sapida@gmail.com>
2016-05-12 10:12:56 -0700

    Release 1: DRY up Teacher and ApprenticeTeacher classes
    - Create a subclass of DbcMember called DbcTeacher and make Teacher and
      ApprenticeTeacher classes inherit from it.
    - Create another initialize method in DbcTeacher and set the default phase variable to 3 after calling super.
    - Pull the #set_phase, #salary=, and #receive_salary methods to DbcTeacher.
    - Pull the #set_performance_rating method from both subclasses into DbcTeacher
      and added type checks to if and elsif branches.

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,17 +1,10 @@
-require_relative 'dbc_member'
+require_relative 'dbc_teacher'
 
-class ApprenticeTeacher < DbcMember
-  attr_reader :salary, :target_raise
+class ApprenticeTeacher < DbcTeacher
 
   def initialize(options={})
     super
-    @phase = 3
     @target_raise = 800
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -19,26 +12,6 @@ class ApprenticeTeacher < DbcMember
     response += "Listen, class, this is how everything works. "
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
-    end
     response
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,17 +1,11 @@
-require_relative 'dbc_member'
+require_relative 'dbc_teacher'
 
-class Teacher < DbcMember
-  attr_reader :salary, :performance_rating, :target_raise
+class Teacher < DbcTeacher
+  attr_reader :performance_rating
 
   def initialize(options={})
     super
-    @phase = 3
     @target_raise = 1000
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -19,27 +13,6 @@ class Teacher < DbcMember
     response += "Listen, class, this is how everything works, fo SHO! "
     response += "*drops flat-out insane knowledge bomb* "
     response += "... You're welcome. *saunters away*"
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      receive_raise(@target_raise)
-      response = "Yay, I'm a great employee!"
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
     response
   end
 end
