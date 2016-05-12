4952361a8cbac11893036768b22e0ccce38a544f
Shirley Rozenboim <email": null>
2016-05-12 10:32:06 -0700

    Did the checkpoint challenge

--- bonus.md
+++ bonus.md
@@ -3,10 +3,12 @@
 Edit this file to answer the following questions. Give examples. You may need to do a bit of research. :)
 
 1. Why is Hash.fetch useful?
+Hash.fetch is useful because it lets you grab a specific key-value pair from the hash.
 
 2. What is the disadvantage of single inheritance, and what's a possible solution to that problem?
 
 3. Why are these classes initialized with an options hash?
+
 
 4. What is the purpose of the private keyword in a class? What does it protect you from and why is that valuable?
 
--- teacher.rb
+++ teacher.rb
@@ -1,21 +1,16 @@
-class Teacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  attr_accessor :name
+require_relative 'teachers'
+require_relative 'all_people'
+
+class Teacher < Teachers
+
+  TARGET_RAISE = 1000
+  MIN_RATING = 90
+
+  attr_reader :performance_rating
 
   def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
-    @target_raise = 1000
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
+    super
+    @target_raise = TARGET_RAISE
   end
 
   def teach_stuff
@@ -26,18 +21,9 @@ class Teacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
   def set_performance_rating(rating)
     response = ""
-    if rating > 90
+    if rating > MIN_RATING
       receive_raise(@target_raise)
       response = "Yay, I'm a great employee!"
     else
