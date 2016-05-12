e3b65498fb6846a2c8abb15f39540c7383bdb529
Ben Goldstein <email": null>
2016-05-12 10:34:41 -0700

    tabbed

--- educator.rb
+++ educator.rb
@@ -20,25 +20,25 @@ class Educator < Person
 		response
 	end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
+	def salary=(new_salary)
+		puts "This better be good!"
+		@salary = new_salary
+	end
 
-    def receive_raise(raise)
-    @salary += raise
-  end
+	def receive_raise(raise)
+		@salary += raise
+	end
 
-    def set_performance_rating(rating)
-    response = ""
-    if rating > self.class::MIN_RATING
-      receive_raise(@target_raise)
-      response = "Yay, I'm a great employee!"
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
+	def set_performance_rating(rating)
+		response = ""
+		if rating > self.class::MIN_RATING
+			receive_raise(@target_raise)
+			response = "Yay, I'm a great employee!"
+		else
+			response += "Oh, well -- thanks to this actionable, specific, and kind "
+			response += "feedback, I'll do better next time."
+		end
+		response
+	end
 end
 
